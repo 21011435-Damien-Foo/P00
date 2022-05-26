639462f7cd53f3090804a798e01f83168caf9154
damie <damie@LAPTOP-J9BDFEPB>
2022-05-26 15:02:20 +0800

    1st

---
+++ P00/Calculator2.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package C206_L06_P00;
+/**
+ * @author damien
+ *
+ */
+public class Calculator {
+	public int add(int a , int b) {
+		return a+b;
+	}
+	public int subtract(int a, int  b) {
+		return a-b;
+	}
+	public int multiply(int a, int b) {
+		return a*b;
+	}
+	public int divide(int a, int b) {
+		return a/b;
+	}
+}
